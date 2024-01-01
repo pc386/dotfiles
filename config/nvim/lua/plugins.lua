c73618ae8342889ced895b43a18bbb0e6da56353
pc386 <petar@cekic.xyz>
2024-01-01 14:57:45 +0100

    nvim

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -3,9 +3,13 @@ return {
 	"nvim-neo-tree/neo-tree.nvim",
     	branch = "v3.x",
     	dependencies = {
-    	  "nvim-lua/plenary.nvim",
-      	"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      	"MunifTanjim/nui.nvim",
-    	}
-}
+    	 	 "nvim-lua/plenary.nvim",
+      		"nvim-tree/nvim-web-devicons",
+      		"MunifTanjim/nui.nvim",
+    		}
+	},
+	{
+	"neoclide/coc.nvim",
+	branch = "release",
+	}		
 }
